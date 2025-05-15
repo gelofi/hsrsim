46b2f5f1b342f57bf10807cb0b87b7d1f1639964
Gelo <gelogelofi@gmail.com>
2025-05-15 19:51:22 +0800

    Added new line

--- src/sections/Hero/Usage/Usage.jsx
+++ src/sections/Hero/Usage/Usage.jsx
@@ -9,7 +9,8 @@ function Usage() {
                 SPD is a stat that determines characters' Action Value. Characters' SPDs are usually around 100-160, depending on your build. <br/><br/>
                 We can calculate the action value with the formula: 10000 / SPD <br/><br/>
                 In this game, high SPD characters are a PRIORITY in taking action.<br/>
-                High SPD results to low AV. The lower the Action Value is, the faster the character arrives in the queue.
+                High SPD results to low AV.<br/>
+                The lower the Action Value is, the faster the character arrives in the queue.
             </p>
         </div>
         </section>
